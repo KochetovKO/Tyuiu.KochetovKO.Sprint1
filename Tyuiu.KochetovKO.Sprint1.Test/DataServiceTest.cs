abf29d1dabb169d98f59b4fe68c260fb88da6390
Кирилл Кочетов <Tyuiu.KochetovKO@outlook.com>
2025-09-17 20:36:26 +0500

    Add project files.

---
+++ Tyuiu.KochetovKO.Sprint1.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.KochetovKO.Sprint1.V0.Lib;
+
+namespace Tyuiu.KochetovKO.Sprint1.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            var res = ds.Calculate();
+            Assert.AreEqual(2, res);
+
+        }
+    }
+}
---
+++ Tyuiu.KochetovKO.Sprint1.V20/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.KochetovKO.Sprint1.V0.Lib;
+namespace Tyuiu.KochetovKO.Sprint1.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт 1 | выполнил: Кочетов К.О. | ИСПБ-25-1";
+            Console.WriteLine("********************************************************************");
+            Console.WriteLine("Спринт №1                                                           ");
+            Console.WriteLine("Тема: Базовые навыки работы в C#                                    ");
+            Console.WriteLine("**********************************************                      ");
+            Console.WriteLine("Задание №0                                                          ");
+            Console.WriteLine("Вариант №22                                                         ");
+            Console.WriteLine("Выполнил: Кочетов Кирилл Олегович | ИСПБ-25-1                       ");
+            Console.WriteLine("********************************************************************");
+            Console.WriteLine("УСЛОВИЕ:                                                            ");
+            Console.WriteLine("Написать программу которая вычисляет выражение 10 + 9 / 3           ");
+            Console.WriteLine("и печатает результат на экране.                                     ");
+            Console.WriteLine("********************************************************************");
+            Console.WriteLine("ИСХОДНЫЕ ДАННЫЕ :                                                   ");
+            Console.WriteLine("********************************************************************");
+            Console.WriteLine("10 + 9 / 3                                                          ");
+            Console.WriteLine("********************************************************************");
+            Console.WriteLine("РЕЗУЛЬТАТ :                                                         ");
+            Console.WriteLine("********************************************************************");
+
+            Console.WriteLine(ds.Calculate());
+
+            Console.ReadLine();
+        }
+
+    }
+}
