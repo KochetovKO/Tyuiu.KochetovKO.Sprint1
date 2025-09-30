564a93a16228d1955ee998e2c828d8a6ebff74c9
Кирилл Кочетов <Tyuiu.KochetovKO@outlook.com>
2025-09-30 13:42:57 +0500

    Task7V10

--- Tyuiu.KochetovKO.Sprint1.Task7.V10.Lib/DataService.cs
+++ Tyuiu.KochetovKO.Sprint1.Task7.V10.Lib/DataService.cs
@@ -5,7 +5,7 @@ namespace Tyuiu.KochetovKO.Sprint1.Task7.V10.Lib
     {
         public double Calculate(double x)
         {
-            double z = 2 * (1 / Math.Tan(3 * x)) - (Math.Log(x) * Math.Cos(x)) / (Math.Log(1 + Math.Pow(x, 2)));
+            double z = 2 * (Math.Cos(3*x) / Math.Sin(3*x)) - Math.Log10(Math.Cos(x)) / (Math.Log10(1 + Math.Pow(x, 2)));
             double q = Math.Round(z, 3);
             return q;
         }
